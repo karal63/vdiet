e86b4e5567a49665303083d40557a580a4052a68
Leo <kuzmichleva1@gmail.com>
2025-05-30 20:56:51 +0200

    Styles for home page

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -6,19 +6,21 @@ import SimpleNavbar from "../components/SimpleNavbar.vue";
     <div class="bg-mainLightBg h-[100vh]">
         <SimpleNavbar />
 
-        <div class="mt-20 ml-40">
-            <p class="text-2xl w-[400px]">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos,
-                fuga. Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Veniam laudantium sequi error fugit? Optio odio sint
-                necessitatibus nobis possimus amet?
+        <div class="mt-40 flex flex-col items-center">
+            <p class="text-2xl w-[400px] text-center">
+                Introducing Vdiet, the cutting-edge fitness app designed to
+                revolutionize your health journey.
             </p>
-            <RouterLink to="/login" class="block mt-5">
+            <RouterLink to="/login" class="block mt-10">
                 <a
-                    class="bg-avocado-500 px-5 py-2 text-xl rounded-xl text-white hover:bg-avocado-600"
+                    class="bg-avocado-500 px-10 py-2 text-xl rounded-xl text-white hover:bg-avocado-600"
                     >Log in</a
                 >
             </RouterLink>
         </div>
+
+        <footer class="absolute w-full flex justify-center bottom-10 left-0">
+            <p>©2025 Vdiet. All rights reserved.</p>
+        </footer>
     </div>
 </template>
